6fd95d93ae704ff89f31d6beeb6d0f0e7bc8f3e4
Bepe <Bepe@home>
2020-05-01 15:02:50 -0300

    Alarm sound

--- jmsdomotic/src/main/resources/arduino.cc
+++ jmsdomotic/src/main/resources/arduino.cc
@@ -1,4 +1,4 @@
-/*
+ /*
   Simple example for receiving
   
   https://github.com/sui77/rc-switch/
@@ -8,9 +8,16 @@
 
 RCSwitch mySwitch = RCSwitch();
 
+int ledPin =  13; //atribui o pino 13 a variável ledPin 
+int buzzerPin =  5; 
+char data;
+bool isSoundActive = false;
+
 void setup() {
   Serial.begin(9600);
   mySwitch.enableReceive(0);  // Receiver on interrupt 0 => that is pin #2
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
 }
 
 void loop() {
@@ -21,7 +28,7 @@ void loop() {
     if (value == 0) {
       Serial.print("Unknown encoding");
     } else { 
-      Serial.print("{\"received\":");
+      Serial.print("{\"data\":");
       Serial.print( mySwitch.getReceivedValue() );
       Serial.print(", \"bits\":");
       Serial.print( mySwitch.getReceivedBitlength() );
@@ -32,4 +39,29 @@ void loop() {
 
     mySwitch.resetAvailable();
   }
+
+  
+  if (Serial.available() > 0) { //verifica se existe comunicação com a porta serial
+      data = Serial.read();//lê os dados da porta serial
+      if (data == '1') {
+        isSoundActive = true;
+        for (int i = 0; i < 5; i++) {
+          analogWrite(buzzerPin, 200);
+          delay(500);
+          analogWrite(buzzerPin, 0);
+          delay(1000);
+        }
+      } else if (data == '2') {
+        isSoundActive = false;
+        analogWrite(buzzerPin, 0);
+      }
+  }
+  
+   if ( isSoundActive ) {
+      analogWrite(buzzerPin, 200);
+      delay(100);
+      analogWrite(buzzerPin, 25);
+      delay(100);
+   }
+
 }
